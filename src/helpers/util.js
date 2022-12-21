7f32bb75ada3aefc8457d2212908bb86745bc62b
Alberto Basabe <Alberto_12ba@hotmail.es>
2022-12-21 11:28:32 -0400

    agregando funcionalidades de fecha

--- src/helpers/util.js
+++ src/helpers/util.js
@@ -1,11 +1,10 @@
-import dayjs from 'dayjs'
-export function getDiffFecha(fecha,diff){
-    const date1 =    dayjs()
+import dayjs from "dayjs";
+export function getDiffFecha(fecha, diff) {
+  const date1 = dayjs();
 
-    return date1.diff(fecha,diff)
+  return date1.diff(fecha, diff);
 }
 
-export function getFormatDate(fecha,format ='hh:m A'){
-
-    return dayjs(fecha).format(format)
+export function getFormatDate(fecha, format = "hh:m A") {
+  return dayjs(fecha).format(format);
 }
--- src/pages/Inicio/Components/CardPaciente/index.js
+++ src/pages/Inicio/Components/CardPaciente/index.js
@@ -1,31 +1,41 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { getDiffFecha, getFormatDate } from "../../../../helpers/util";
 
 import "./index.css";
 
-export const CardPaciente = ({admision}) => {
+export const CardPaciente = ({ admision }) => {
+  const navigate = useNavigate();
+  const handlerDoubleClick = () => {
+    navigate(`/admision/${admision.documento.numero_factura}/consulta`);
+  };
   return (
-    <article className="cardPac__container" >
+    <article className="cardPac__container" onDoubleClick={handlerDoubleClick}>
       <div className="cardPac__indicator"></div>
       <div className="cardPac__containerData">
         <div className="cardPac__persona">
           <div className="cardPac__data">
             <p>{admision.documento.numero_factura}</p>
-            <p>{admision.documento.numero_identificacion.valor_identificacion}</p> 
+            <p>
+              {admision.documento.numero_identificacion.valor_identificacion}
+            </p>
           </div>
           <p className="cardPac_name">{`${admision.documento.persona.nombre} ${admision.documento.persona.apellido}`}</p>
         </div>
-        <p>{`${getDiffFecha(admision.documento.persona.fecha_nacimiento,'year')} años`}</p>
+        <p>{`${getDiffFecha(
+          admision.documento.persona.fecha_nacimiento,
+          "year"
+        )} años`}</p>
         <p>{admision.documento.persona.sexo}</p>
-        <p className="cardPac__diagnostico">{admision.documento.diagnostico ? admision.documento.diagnostico : '' }</p>
+        <p className="cardPac__diagnostico">
+          {admision.documento.diagnostico ? admision.documento.diagnostico : ""}
+        </p>
         <p className="cardPac__cama">{admision.cama.descripcion}</p>
         <div>
-          <p>{getFormatDate(admision.fecha_de_atencion,'YYYY-MM-DD')}</p>
+          <p>{getFormatDate(admision.fecha_de_atencion, "YYYY-MM-DD")}</p>
           <p>{getFormatDate(admision.fecha_de_atencion)}</p>
         </div>
-        <p className="cardPac_cliente">
-          {admision.documento.nombre_cliente}
-        </p>
+        <p className="cardPac_cliente">{admision.documento.nombre_cliente}</p>
       </div>
     </article>
   );
